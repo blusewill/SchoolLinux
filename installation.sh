b8af2cb8bfde57f12ecf756f48a6d2d7bd11a1e1
blusewill <62820836+blusewilll@users.noreply.github.com>
2022-01-25 22:57:01 +0800

    installation.sh update

--- installation.sh
+++ installation.sh
@@ -13,3 +13,20 @@ systemctl enable NetworkManager
 
 yes | pacman -Sy wget
 wget https://raw.githubusercontent.com/blusewilll/SchoolLinux/main/default.conf -O /usr/lib/sddm/sddm.conf.d/default.conf
+
+# Install yt-dlp
+
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+chmod a+rx /usr/local/bin/yt-dlp
+
+# Install snap
+
+git clone https://aur.archlinux.org/snapd.git
+cd snapd
+makepkg -si
+systemctl enable --now snapd.socket
+ln -s /var/lib/snapd/snap /snap
+
+# Install Flatpak
+
+pacman -S flatpak
--- user.sh
+++ user.sh
@@ -7,3 +7,40 @@ sudo chown -R $USER:$USER ./yay-git
 cd yay-git
 yes | makepkg -si
 yay -Syu
+
+
+# Install Brave Web Broswer
+
+yes | yay -S brave-bin
+i
+# Install LibreOffice (Still)
+
+yes | yay -S libreoffice-Still
+
+# Install [Upgrade] VLC
+
+yes | yay -S vlc
+
+# Install mpv
+yes | yay -S mpv
+
+# Install [Upgrade] ffmpeg
+
+yes | yay -S ffmpeg
+
+
+# Install VSC (Visual Studio Code)
+
+yay -S visual-studio-code-bn --noconfirm
+
+# Install arduino
+
+sudo snap install arduino
+
+# Install Scratch
+
+flatpak install flathub edu.mit.Scratch
+
+# Install Pipe-viewer
+
+yay -S pipe-viewer-git --noconfirm
