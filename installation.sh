6e0b3114dd2896adaa599f06142e081c1053611b
blusewill <62820836+blusewilll@users.noreply.github.com>
2022-01-25 21:19:40 +0800

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,4 +1,17 @@
+# System Update
 yes | pacman -Syu 
-yes | pacman --needed xorg sddm plasma kde-applications
+
+#install plasma
+pacman --needed xorg sddm plasma kde-applications --noconfirm
+
+#enable System Boot by Default
+
 systemctl enable sddm
 systemctl enable NetworkManager
+
+#Copy sddm (login manager) setttings
+
+yes | pacman -Sy wget
+wget https://raw.githubusercontent.com/blusewilll/SchoolLinux/main/default.conf
+rm /usr/lib/sddm/sddm.conf.d/default.conf
+mv ~/default.conf /usr/lib/sddm/sddm.conf.d/default.conf
